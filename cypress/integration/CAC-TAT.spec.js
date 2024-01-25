15bf54a15a0c7b955270de6b0e47085d3ee7ac75
Sidnei <sidneibaltazar1998@gmail.com>
2024-01-25 11:15:31 -0300

    first commit

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(function() {
+        cy.visit('./src/index.html');
+    })
+
+    it('verifica o título da aplicação', function() {
+
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+    
+    })
+
+    it.only('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName').type('Sidnei')
+        cy.get('#lastName').type('Junior')
+        cy.get('#email').type('email@gmail.com')
+        cy.get('#open-text-area').type('Texto padrão')
+        cy.get('button[type="submit"]').click()
+    })
+
+
+    
+  })
